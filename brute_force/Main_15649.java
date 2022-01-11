04b506ebd2f77a694bb8979f03f8b58c4092316a
jjb8966 <jjb8966@naver.com>
2022-01-12 02:01:50 +0900

    chore: 주석 추가, 변수명 수정 및 패키지명 변경

--- brute_force/Main_15649.java
+++ brute_force/Main_15649.java
@@ -1,13 +1,29 @@
-package bruteforce;
+package brute_force;
 
 import java.util.Scanner;
 
-//중복x, 순서o -> O(N P M) = N! / (N-M)!
+/**
+ * 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+ * 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+ * 1 ≤ M ≤ N ≤ 8
+ * [입력]
+ * 3 2
+ * [출력]
+ * 1 2
+ * 1 3
+ * 2 1
+ * 2 3
+ * 3 1
+ * 3 2
+ */
+
+// 중복x, 순서o -> 시간복잡도 : O(N P M) = N! / (N-M)!
 public class Main_15649 {
-    static StringBuilder sb = new StringBuilder();
-    static int N, M;
-    static int[] values;
-    static int[] used;
+
+    private static StringBuilder sb = new StringBuilder();
+    private static int N, M;
+    private static int[] values;
+    private static int[] used;
 
     private static void input() {
         Scanner sc = new Scanner(System.in);
@@ -17,32 +33,33 @@ public class Main_15649 {
         used = new int[N + 1];        //1부터 N까지의 숫자중 사용된 것이 있으면 1, 없으면 0을 저장
     }
 
-    public static void recurrenceFunction(int k) {
-        if (k == M + 1) {
+    public static void recurrenceFunction(int digit) {
+        // 1. 탐색이 끝난 경우
+        if (digit == M + 1) {
             for (int i = 1; i <= M; i++) {
-                sb.append(values[i] + " ");
+                sb.append(values[i] + " ");     // 1번째 자리값 + 2번째 자리값 + ... + M번째 자리값
             }
             sb.append("\n");
-        } else {
-            for (int cand = 1; cand <= N; cand++) {
-                if (used[cand] == 1) {
+        } else {        // 2. 탐색이 남은 경우
+            for (int candidate = 1; candidate <= N; candidate++) {
+                if (used[candidate] == 1) {     // candidate가 사용된 적이 있는 숫자이면 pass
                     continue;
                 }
 
-                values[k] = cand;
-                used[cand]++;
+                values[digit] = candidate;
+                used[candidate]++;
 
-                recurrenceFunction(k + 1);
+                recurrenceFunction(digit + 1);
 
-                values[k] = 0;
-                used[cand]--;
+                values[digit] = 0;      // 재귀함수의 마지막 함수가 호출된 후 반드시 초기화를 해줘야함
+                used[candidate]--;
             }
         }
     }
 
     public static void main(String[] args) {
         input();
-        recurrenceFunction(1);      //1번째 자리부터 올바른 원소를 고르는 함수\
+        recurrenceFunction(1);      //1번째 자리부터 올바른 원소를 고르는 함수
         System.out.println(sb);
     }
 }
--- brute_force/Main_15651.java
+++ brute_force/Main_15651.java
@@ -1,12 +1,31 @@
-package bruteforce;
+package brute_force;
 
 import java.util.Scanner;
 
+/**
+ * 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+ *  1부터 N까지 자연수 중에서 M개를 고른 수열
+ *  같은 수를 여러 번 골라도 된다.
+ *  1 ≤ M ≤ N ≤ 7
+ *  [입력]
+ *  3 2
+ *  [출력]
+ * 1 1
+ * 1 2
+ * 1 3
+ * 2 1
+ * 2 2
+ * 2 3
+ * 3 1
+ * 3 2
+ * 3 3
+ */
 //중복o , 순서o -> O(N^M)
 public class Main_15651 {
-    static StringBuilder sb = new StringBuilder();
-    static int N, M;
-    static int[] values;
+
+    private static StringBuilder sb = new StringBuilder();
+    private static int N, M;
+    private static int[] values;
 
     private static void input() {
         Scanner sc = new Scanner(System.in);
@@ -16,19 +35,20 @@ public class Main_15651 {
     }
 
     //k번째 자리부터 조건에 맞는 원소를 고르는 재귀 함수
-    private static void recurrenceFunction(int k) {
-        if (k == M + 1) {       //탐색 끝
+    private static void recurrenceFunction(int digit) {
+        // 1. 탐색이 끝난 경우
+        if (digit == M + 1) {
             for (int i = 1; i <= M; i++) {
                 sb.append(values[i] + " ");
             }
             sb.append("\n");
-        } else {                //k번째 자리에 올 수 있는 숫자 대입 후 k+1
-            for (int cand = 1; cand <= N; cand++) {
-                values[k] = cand;
+        } else {                // 2. 탐색이 남은 경우
+            for (int candidate = 1; candidate <= N; candidate++) {     //k번째 자리에 올 수 있는 숫자 대입 후 k+1
+                values[digit] = candidate;
 
-                recurrenceFunction(k + 1);
+                recurrenceFunction(digit + 1);
 
-                values[k] = 0;
+                values[digit] = 0;
             }
         }
     }
